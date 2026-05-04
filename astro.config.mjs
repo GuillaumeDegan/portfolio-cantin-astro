9a2c663258a203fe6b018620e526923b82d7bc57
GuillaumeDegan <guillaume.degan@free.fr>
2026-05-04 19:17:03 +0200

    astro config with vercel fix attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  output: "static",
+  //   adapter: vercel(),
 });
