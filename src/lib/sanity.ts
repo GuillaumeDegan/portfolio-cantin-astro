1430536d2232f3438b3f32d19ff5a1861d41a898
GuillaumeDegan <guillaume.degan@free.fr>
2026-05-09 14:09:55 +0200

    astro config with vercel fix attempt

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,6 +3,6 @@ import { createClient } from "@sanity/client";
 export const sanity = createClient({
   projectId: "fhumllcy",
   dataset: "production",
-  apiVersion: "2026-03-29",
+  apiVersion: "2024-01-01",
   useCdn: false,
 });
